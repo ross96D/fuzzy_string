e3df2eb3b4d603b8eec022f84dd1f834fa4d8a77
ross96D <abelaross96@gmail.com>
2025-06-16 17:31:08 -0400

    add some test cases

--- test/fuzzy_string_test.dart
+++ test/fuzzy_string_test.dart
@@ -1,16 +1,22 @@
 import 'package:fuzzy_string/fuzzy_string.dart';
-import 'package:test/test.dart';
+import 'package:test/test.dart' as tst;
 
 void main() {
-  group('A group of tests', () {
+  tst.group('A group of tests', () {
     final damerauLevenshtein = DamerauLevenshtein();
+    final smithWaterman = SmithWaterman();
 
-    test('Case sensitive', () {
-      expect(damerauLevenshtein.similarity('casa', 'Casa'), 1);
+    tst.test('Case sensitive', () {
+      tst.expect(damerauLevenshtein.similarity('casa', 'Casa'), 1);
     });
 
-    test('Case insensitive', () {
-      expect(damerauLevenshtein.similarityIgnoreCase('casa', 'Casa'), 0);
+    tst.test('Case insensitive', () {
+      tst.expect(damerauLevenshtein.similarityIgnoreCase('casa', 'Casa'), 0);
+    });
+
+    tst.test("team case", () {
+      tst.expect(smithWaterman.normalSimilarityIgnoreCase('Steam', 'teams'), tst.greaterThan(0.5));
+      tst.expect(smithWaterman.normalSimilarityIgnoreCase('Microsoft Teams for Linux', 'teams'), tst.equals(1));
     });
   });
 }
